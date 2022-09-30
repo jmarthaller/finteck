a80191a42aca29ba6be3295a1f801a048496b368
Jacob Marthaller <marthaller.jr@gmail.com>
2022-09-30 10:10:45 -0700

    Fetch NOAA data example

--- finteck-frontend/src/components/App.tsx
+++ finteck-frontend/src/components/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from "./Header";
 // import BeachesContainer from "./BeachesContainer";
 import Search from "./Search";
 import UserProfile from "./UserProfile";
 
-function App() {
+const App = () => {
+  // check alt file for key
+  const NOAA_KEY = undefined;
 
+  useEffect(() => {
+    const fetchDataOnLaunch = async () => {
+      const datasetData = await fetch(
+        `https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories?offset=20`,
+        {
+          headers: {
+            token: `${NOAA_KEY}`,
+          },
+        }
+      );
+      const jsonifiedData = await datasetData.json();
+      console.log(jsonifiedData);
+    }
+    fetchDataOnLaunch();
+  }, [])
 
 
   return (

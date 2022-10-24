0e0e7dfc4a04ca3b1e245f1f6b98d9bbe7406d5b
Jacob Marthaller <marthaller.jr@gmail.com>
2022-10-24 15:52:25 -0700

    Delete cruft from express.ts

--- server/routes/record.ts
+++ server/routes/record.ts
@@ -1,4 +1,4 @@
-// // const express = require("express");
+// const express = require("express");
 
 // // recordRoutes is an instance of the express router.
 // // We use it to define our routes.
@@ -6,7 +6,7 @@
 // const recordRoutes = express.Router();
 
 // // This will help us connect to the database
-// // const dbo = require("../db/conn");
+// const dbo = require("../db/conn");
 
 // // This help convert the id from string to ObjectId for the _id.
 // const ObjectId = require("mongodb").ObjectId;

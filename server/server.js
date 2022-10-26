e35bace6babfa1415bcca7ac25c7ae88b3db7286
Jacob Marthaller <marthaller.jr@gmail.com>
2022-10-25 19:04:36 -0700

    clean up unnecessary comments in server.js

--- server/server.js
+++ server/server.js
@@ -6,11 +6,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-// get driver connection
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
-  // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
   });

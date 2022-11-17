a54b6df340a8edea84653be769e731f6828e93c9
Jacob Marthaller <marthaller.jr@gmail.com>
2022-11-16 19:06:07 -0800

    Modify NavBar, implement signout

--- finteck-frontend/src/components/NavBar.tsx
+++ finteck-frontend/src/components/NavBar.tsx
@@ -18,7 +18,7 @@ import {
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-const Links = ['Home', 'Favorites', 'Profile'];
+const Links = ['Home', 'Favorites', 'Profile', 'Sign Out'];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -70,7 +70,7 @@ export default function Simple() {
                 <Avatar
                   size={'sm'}
                   src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    ''
                   }
                 />
               </MenuButton>

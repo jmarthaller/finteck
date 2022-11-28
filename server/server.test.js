a392f0e3f66622531819740755ea1918997512c5
Jacob Marthaller <marthaller.jr@gmail.com>
2022-11-28 10:11:19 -0800

    Add tests for data model

--- server/server.test.js
+++ server/server.test.js
@@ -1,7 +1,43 @@
-function sum(a, b) {
-  return a + b;
-}
+const dataModel = [
+  {
+    _id: "[SAMPLE_ID_AUTO_GEN_FROM_MONGO",
+    name: "Bolsa Chica",
+    position: "Orange County",
+    level: "Standard",
+    confidenceScore: 0.75,
+    mapSRCForIframe: null,
+  },
+];
 
-test("sums two numbers accurately", () => {
-  expect(sum(1, 2)).toBe(3);
+test("validates id for beach data model", () => {
+  expect(dataModel[0]._id).not.toBeUndefined();
+});
+
+test("validates name for beach data model", () => {
+  expect(typeof dataModel[0].name).toBe("string");
+
+  expect(dataModel[0].name.length).toBeGreaterThan(0);
+});
+
+test("validates position for beach data model", () => {
+  expect(typeof dataModel[0].position).toBe("string");
+
+  expect(dataModel[0].position.length).toBeGreaterThan(0);
+});
+
+test("validates level for beach data model", () => {
+  expect(typeof dataModel[0].level).toBe("string");
+
+  expect(dataModel[0].level.length).toBeGreaterThan(0);
+});
+
+test("validates confidenceScore for beach data model", () => {
+  expect(typeof dataModel[0].confidenceScore).toBe("number");
+
+  expect(dataModel[0].confidenceScore).toBeGreaterThan(0);
+  expect(dataModel[0].confidenceScore).toBeLessThan(1);
+});
+
+test("validates iframe for beach data model", () => {
+  expect(typeof dataModel[0].mapSRCForIframe).toBeTruthy();
 });
